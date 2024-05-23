656392294292eb3c95699c36ede5080f48ca3e22
shivamsancc <shivam.anand@superbot.one>
2024-05-23 18:36:06 +0530

    ads

---
+++ node_js/a.js
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker';
+import http from 'http';
+
+export function createRandomUser() {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+  };
+}
+
+export const a = faker.helpers.multiple(createRandomUser, {
+  count: 5,
+});
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(a));
+}).listen(8080);
