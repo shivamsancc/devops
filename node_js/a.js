1ec7d55cc0148c0f0c9b5aa384bc577fe8ecfd6b
shivamsancc <shivam.anand@superbot.one>
2024-05-24 21:00:33 +0530

    aa

--- node_js/a.js
+++ node_js/a.js
@@ -1,23 +1,7 @@
 
 import http from 'http';
 
-// export function createRandomUser() {
-//   return {
-//     userId: faker.string.uuid(),
-//     username: faker.internet.userName(),
-//     email: faker.internet.email(),
-//     avatar: faker.image.avatar(),
-//     password: faker.internet.password(),
-//     birthdate: faker.date.birthdate(),
-//     registeredAt: faker.date.past(),
-//     registeredAt: faker.date.new(),
-//     registeredAtDD: faker.date.new(),
-//   };
-// }
-
-// export const a = faker.helpers.multiple(createRandomUser, {
-//   count: 5012,
-// });
+console.log(' i m running in cluster');
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
