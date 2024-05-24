61e7b0ee11fed37b15eed34b04efb4b1037da339
shivamsancc <shivam.anand@superbot.one>
2024-05-24 20:53:20 +0530

    adf

--- node_js/a.js
+++ node_js/a.js
@@ -1,23 +1,23 @@
-import { faker } from '@faker-js/faker';
+
 import http from 'http';
 
-export function createRandomUser() {
-  return {
-    userId: faker.string.uuid(),
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-    avatar: faker.image.avatar(),
-    password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
-    registeredAt: faker.date.new(),
-    registeredAtDD: faker.date.new(),
-  };
-}
+// export function createRandomUser() {
+//   return {
+//     userId: faker.string.uuid(),
+//     username: faker.internet.userName(),
+//     email: faker.internet.email(),
+//     avatar: faker.image.avatar(),
+//     password: faker.internet.password(),
+//     birthdate: faker.date.birthdate(),
+//     registeredAt: faker.date.past(),
+//     registeredAt: faker.date.new(),
+//     registeredAtDD: faker.date.new(),
+//   };
+// }
 
-export const a = faker.helpers.multiple(createRandomUser, {
-  count: 5012,
-});
+// export const a = faker.helpers.multiple(createRandomUser, {
+//   count: 5012,
+// });
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
