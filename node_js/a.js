7d12fb3486633c907d641107349bd73b5d7d6960
shivamsancc <shivam.anand@superbot.one>
2024-05-27 19:32:20 +0530

    asd

--- node_js/a.js
+++ node_js/a.js
@@ -2,7 +2,7 @@ import http from 'http';
 
 console.log('Im running in a cluster');
 
-const a = {}; // Define an empty object
+const a = {'Im running in a cluster'}; // Define an empty object
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
