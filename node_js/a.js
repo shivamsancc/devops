fd661f4652f26f92df5ef21920f9bbe36fb98daf
shivamsancc <shivam.anand@superbot.one>
2024-05-27 19:46:00 +0530

    asdf

--- node_js/a.js
+++ node_js/a.js
@@ -3,10 +3,11 @@ import http from 'http';
 console.log('I\'m running in a cluster');
 
 const a = { message: 'Im running in a cluster' }; // Define an object with a property
+const dta = { message: 'Im running in a cluster created by shivam' }; // Define an object with a property
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(a));
+  res.end(JSON.stringify(dta));
 }).listen(8000);
 
 http.createServer(function (req, res) {
