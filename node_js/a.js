ededc7ad67786007cb1dd16b9cfe2c43ff9d51cc
shivamsancc <shivam.anand@superbot.one>
2024-05-27 19:14:49 +0530

    a

--- node_js/a.js
+++ node_js/a.js
@@ -7,3 +7,9 @@ http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify(a));
 }).listen(8000);
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(a));
+}).listen(32025);
